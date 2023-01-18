bca4c00f9f7bc89f6d85e5d3b5e1cbd1baa1d6e7
paulmarniquet <paul.marniquet@epitech.eu>
2023-01-18 15:21:23 +0100

    [+] Sandbox Gmail - Create a draft + Send a mail done [+]

---
+++ Create a draft/script.js
@@ -0,0 +1,79 @@
+const fs = require('fs').promises;
+const path = require('path');
+const process = require('process');
+const {authenticate} = require('@google-cloud/local-auth');
+const {google} = require('googleapis');
+
+const SCOPES = ['https://www.googleapis.com/auth/gmail.compose'];
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
+const {Command} = require('commander');
+const program = new Command();
+
+program
+    .usage('[options]')
+    .option('-t, --to <to>', 'To email address')
+    .option('-s, --subject <subject>', 'Subject')
+    .option('-b, --body <body>', 'Body')
+    .parse(process.argv);
+
+const options = program.opts();
+
+async function loadSavedCredentialsIfExist() {
+    try {
+        const content = await fs.readFile(TOKEN_PATH);
+        const credentials = JSON.parse(content);
+        return google.auth.fromJSON(credentials);
+    } catch (err) {
+        return null;
+    }
+}
+
+async function saveCredentials(client) {
+    const content = await fs.readFile(CREDENTIALS_PATH);
+    const keys = JSON.parse(content);
+    const key = keys.installed || keys.web;
+    const payload = JSON.stringify({
+        type: 'authorized_user',
+        client_id: key.client_id,
+        client_secret: key.client_secret,
+        refresh_token: client.credentials.refresh_token,
+    });
+    await fs.writeFile(TOKEN_PATH, payload);
+}
+
+async function authorize() {
+    let client = await loadSavedCredentialsIfExist();
+    if (client) {
+        return client;
+    }
+    client = await authenticate({
+        scopes: SCOPES,
+        keyfilePath: CREDENTIALS_PATH,
+    });
+    if (client.credentials) {
+        await saveCredentials(client);
+    }
+    return client;
+}
+
+async function createDrafts(auth) {
+    const gmail = google.gmail({version: 'v1', auth});
+    const message = btoa(
+        "To: " + options.to + "\r\n" +
+        "Subject: " + options.subject + "\r\n\r\n" +
+        options.body);
+    const res = await gmail.users.drafts.create({
+        userId: 'me',
+        requestBody: {
+            id: 'me',
+            message: {
+                raw: message,
+            },
+        },
+    });
+    console.log(res.data);
+}
+
+authorize().then(createDrafts).catch(console.error);
---
+++ Send a mail/README.md
@@ -0,0 +1,25 @@
+1. Allez sur la console Google Cloud (https://console.cloud.google.com/).
+
+
+2. Créez un nouveau projet en cliquant sur le menu déroulant du projet et en sélectionnant ou en créant le projet que vous souhaitez utiliser.
+
+
+3. Une fois que vous avez créé votre projet, allez dans la barre latérale gauche et cliquez sur le bouton "Activer les API et les services".
+
+
+4. Recherchez l'API que vous souhaitez utiliser et cliquez sur le nom de l'API pour accéder à la page de détails de l'API.
+
+
+5. Cliquez sur le bouton "Activer" pour activer l'API.
+
+
+6. Allez dans la barre latérale gauche, cliquez sur l'onglet "Informations d'identification" et cliquez ensuite sur le bouton "Créer des informations d'identification".
+
+
+7. Sélectionnez le type d'informations d'identification que vous souhaitez créer (ID client OAuth) et suivez les instructions pour créer les informations d'identification.
+
+
+8. Une fois les informations d'identification créées, vous devriez pouvoir les télécharger sous forme de fichier JSON. Ce fichier contient les informations nécessaires à l'authentification, telles que l'ID client, le secret client et d'autres détails nécessaires à l'utilisation de l'API.
+
+
+9. Une fois que vous avez téléchargé le fichier JSON, vous devez la renommer "credentials.json" et l'ajouter à votre projet.
